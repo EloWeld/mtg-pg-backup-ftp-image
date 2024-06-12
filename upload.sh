ad98fd65006fd626233d63f99438e75e7a9003cf
jannikhst <58545820+jannikhst@users.noreply.github.com>
2024-06-12 16:52:31 +0200

    add url encoding for password

--- upload.sh
+++ upload.sh
@@ -1,14 +1,29 @@
 #!/bin/sh
 
+# Function to URL-encode input
+urlencode() {
+  local length="${#1}"
+  for (( i = 0; i < length; i++ )); do
+    local c="${1:i:1}"
+    case $c in
+      [a-zA-Z0-9.~_-]) printf "$c" ;;
+      *) printf '%%%02X' "'$c" ;;
+    esac
+  done
+}
+
 # The backup file to be transferred
 BACKUP_FILE=$1
 echo "Starting upload of $BACKUP_FILE"
 
-# Debug: Print full path with obsure password
+# URL-encode the password
+ENCODED_FTP_PASS=$(urlencode "$FTP_PASS")
+
+# Debug: Print full path with obscure password
 echo "curl -T \"$BACKUP_FILE\" \"ftp://$FTP_USER:***@$FTP_HOST/$FTP_PATH/\" --ftp-create-dirs"
 
 # FTP upload
-curl -T "$BACKUP_FILE" "ftp://$FTP_USER:$FTP_PASS@$FTP_HOST/$FTP_PATH/" --ftp-create-dirs
+curl -T "$BACKUP_FILE" "ftp://$FTP_USER:$ENCODED_FTP_PASS@$FTP_HOST/$FTP_PATH/" --ftp-create-dirs
 
 # Check if the upload was successful
 if [ $? -eq 0 ]; then
