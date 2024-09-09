1ac8d5a970758fd9c1e696fcb9bb459a59058627
jannikhst <58545820+jannikhst@users.noreply.github.com>
2024-09-09 19:25:17 +0200

    make logs visibile for docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 # Default to daily if CRON_SCHEDULE is not set
 CRON_SCHEDULE="${CRON_SCHEDULE:-0 2 * * *}"
 
-# Create a new crontab file
-echo "$CRON_SCHEDULE /scripts/backup.sh >> /var/log/cron.log 2>&1" > /etc/crontabs/root
+# Create a new crontab file with output redirected to stdout
+echo "$CRON_SCHEDULE /scripts/backup.sh >> /proc/1/fd/1 2>&1" > /etc/crontabs/root
 
 echo "Cron job schedule: $CRON_SCHEDULE"
 # build the full ftp url
